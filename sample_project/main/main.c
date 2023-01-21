6673d11da932aa1b928483e0c23a9bbaf39e2c7d
Arne <arne@rnae.de>
2023-01-21 17:02:22 +0100

    stuff

--- sample_project/main/main.c
+++ sample_project/main/main.c
@@ -4,52 +4,58 @@
 #include "esp_dmx.h"
 #include "esp_log.h"
 #include "esp_system.h"
-#include "RdmHandler.h"
+#include "esp_rdm_client.h"
+
 
 #define TX_PIN 17 // the pin we are using to TX with
 #define RX_PIN 16 // the pin we are using to RX with
 #define EN_PIN 21 // the pin we are using to enable TX on the DMX transceiver
 
+
 static const char *TAG = "main";
 static uint8_t data[DMX_MAX_PACKET_SIZE] = {};
+static bool identifyOn = false;
+
+void handleRdmEvent(const dmx_packet_t *dmxPacket, const void *data, const uint16_t size);
+
+void startAddressChangedCb(uint16_t newAddr)
+{
+    ESP_LOGI(TAG, "Start addr callback: %d", newAddr);
+}
+
+void personalityCb(uint8_t person)
+{
+    ESP_LOGI(TAG, "personalityCb callback: %d", person);
+}
 
 void dmxTask(void *unused)
 {
     ESP_ERROR_CHECK(dmx_set_pin(DMX_NUM_2, TX_PIN, RX_PIN, EN_PIN));
     ESP_ERROR_CHECK(dmx_driver_install(DMX_NUM_2, DMX_DEFAULT_INTR_FLAGS));
-    initRdmHandler(1);
-    int count = 0;
 
+    // TODO use SP_ERROR_CHECK
+    rdm_client_init(DMX_NUM_2, 42, 13, "LICHTAUSGANG Blinder", "default");
+    rdm_client_add_personality(DMX_NUM_2, 42, "other footprint");
+    rdm_client_set_start_address_changed_cb(DMX_NUM_2, startAddressChangedCb);
+    rdm_client_set_personality_changed_cb(DMX_NUM_2, personalityCb);
     while (true)
     {
-        dmx_event_t event;
+        dmx_packet_t dmxPacket;
         size_t ret = 0;
-        ret = dmx_receive(DMX_NUM_2, &event, DMX_TIMEOUT_TICK);
+        ret = dmx_receive(DMX_NUM_2, &dmxPacket, DMX_TIMEOUT_TICK);
         if (ret)
         {
-            // Check that no errors occurred.
-            if (event.err == ESP_OK)
+            if (dmxPacket.err == ESP_OK)
             {
-
-                if (event.is_rdm)
+                if (dmxPacket.is_rdm)
                 {
-                    // ESP_LOGI(TAG, "!GOT RDM. SC: %d, size: %d", event.sc, event.size);
-                    dmx_read(DMX_NUM_2, data, event.size);
-                    handleRdmEvent(&event, data, event.size);
+                    dmx_read(DMX_NUM_2, data, dmxPacket.size);
+                    rdm_client_handle_rdm_message(DMX_NUM_2, &dmxPacket, data, dmxPacket.size);
                 }
-
-                count++;
-
-                // if ((count % 10) == 0)
-                // {
-                //     const double currentTimS = esp_timer_get_time() / 1000000.0;
-                //     // ESP_LOGE(TAG, "%f: hz: %f, ret: %d, data: %d", currentTimS, (count / currentTimS), ret, data[1]);
-                //     // ESP_LOG_BUFFER_HEX(TAG, data, 16);
-                // }
             }
             else
             {
-                ESP_LOGE(TAG, "dmx error: %s", esp_err_to_name(event.err));
+                ESP_LOGE(TAG, "dmx error: %s", esp_err_to_name(dmxPacket.err));
             }
         }
     }
@@ -58,7 +64,6 @@ void dmxTask(void *unused)
 void app_main()
 {
     TaskHandle_t dmxTaskHandle = NULL;
-    // TODO determine real stack usage and reduce later
     xTaskCreatePinnedToCore(dmxTask, "DMX_TASK", 10240, NULL, 2, &dmxTaskHandle, 1);
     if (!dmxTaskHandle)
     {
